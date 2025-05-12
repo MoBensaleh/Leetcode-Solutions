fd3c293766b2b07700a54b31c0bf7aa9ea985437
Moe Bensaleh <61438144+MoBensaleh@users.noreply.github.com>
2025-05-12 12:17:15 -0600

    Create file

---
+++ 1929-ConcatenationofArray/1929-ConcatenationofArray.py
@@ -0,0 +1,8 @@
+# Last updated: 5/12/2025, 12:16:20 PM
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        ans = []
+        for i in range(2):
+            for num in nums:
+                ans.append(num)
+        return ans
