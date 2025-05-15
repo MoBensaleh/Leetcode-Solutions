6296f83626770f605554b1b4effc968a7921af86
Moe Bensaleh <61438144+MoBensaleh@users.noreply.github.com>
2025-05-14 20:22:09 -0600

    File updated at 5/14/2025, 8:22:09 PM

--- 125-ValidPalindrome/125-ValidPalindrome.py
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -1,14 +1,22 @@
-# Last updated: 5/14/2025, 8:20:29 PM
+# Last updated: 5/14/2025, 8:22:09 PM
 class Solution:
-    def mergeAlternately(self, word1: str, word2: str) -> str:
-        idx1 , idx2 = 0,0
-        newStr = ""
-        while idx1 < len(word1) and idx2 < len(word2):
-            newStr += word1[idx1]
-            newStr += word2[idx2]
-            idx1 +=1
-            idx2 +=1
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        last = m+n-1
 
-        newStr += word1[idx1:]
-        newStr += word2[idx2:]
-        return newStr
+        while m>0 and n>0:
+            if nums1[m-1] > nums2[n-1]:
+                nums1[last] = nums1[m-1]
+                m-=1 
+            else:
+                nums1[last] = nums2[n-1]
+                n-=1 
+            last-=1
+        
+        while n>0:
+            nums1[last] = nums2[n-1]
+            n-=1
+            last-=1
+        
