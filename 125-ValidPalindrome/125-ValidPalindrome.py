7c84a0fa713d5241e7035008c371ea4c9b1dfb0a
Moe Bensaleh <61438144+MoBensaleh@users.noreply.github.com>
2025-05-14 20:24:22 -0600

    File updated at 5/14/2025, 8:24:21 PM

--- 125-ValidPalindrome/125-ValidPalindrome.py
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -1,22 +1,9 @@
-# Last updated: 5/14/2025, 8:22:09 PM
+# Last updated: 5/14/2025, 8:24:21 PM
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        last = m+n-1
-
-        while m>0 and n>0:
-            if nums1[m-1] > nums2[n-1]:
-                nums1[last] = nums1[m-1]
-                m-=1 
-            else:
-                nums1[last] = nums2[n-1]
-                n-=1 
-            last-=1
-        
-        while n>0:
-            nums1[last] = nums2[n-1]
-            n-=1
-            last-=1
-        
+    def removeDuplicates(self, nums: list[int]) -> int:
+        l = 1
+        for r in range(1, len(nums)):
+            if nums[r] != nums[r - 1]:
+                nums[l] = nums[r]
+                l += 1
+        return l
