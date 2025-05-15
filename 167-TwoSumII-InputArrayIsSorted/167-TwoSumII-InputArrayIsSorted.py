47b00f1e1031ca83895b0de7293dfe9f8d4ed201
Moe Bensaleh <61438144+MoBensaleh@users.noreply.github.com>
2025-05-14 20:56:09 -0600

    Delete 167-TwoSumII-InputArrayIsSorted directory

--- 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.py
+++
@@ -1,9 +0,0 @@
-# Last updated: 5/14/2025, 8:37:50 PM
-class Solution:
-    def removeDuplicates(self, nums: list[int]) -> int:
-        l = 1
-        for r in range(1, len(nums)):
-            if nums[r] != nums[r - 1]:
-                nums[l] = nums[r]
-                l += 1
-        return l
